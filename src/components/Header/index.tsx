1066004fabd8d25cc669bbc0fab891ab62d2ccd2
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-06-16 11:32:38 -0300

    creation of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export function Header(): JSX.Element {
+  return (
+    <header className={`${commonStyles.container} ${styles.headerContainer}`}>
+      <div className={commonStyles.innerContainer}>
+        <img src="/logo.svg" alt="spacetraveling" width="240" />
+      </div>
+    </header>
+  );
 }
